007d485865de3feff2cb9957bbacd77669181af0
falchizao <marcelonavarro11md@gmail.com>
2022-03-13 10:24:05 -0300

    BootStrap

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,6 +1,7 @@
+import 'bootstrap/dist/css/bootstrap.css';
+import './index.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 ReactDOM.render(
   <React.StrictMode>
