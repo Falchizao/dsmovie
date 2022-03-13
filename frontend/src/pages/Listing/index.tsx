7505e65e55d69c62e4012e3984e065f31b95d947
falchizao <marcelonavarro11md@gmail.com>
2022-03-13 20:52:55 -0300

    Pagination

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -30,13 +30,17 @@ function Listing() {
             })
     }, [pageNumber]);
 
+    const handlePageChange = (newPageNumber : number) =>{
+        setPageNumber(newPageNumber);
+    }
+
 
 
     return (
         <>
 
 
-            <Pagination />
+            <Pagination page = {page} onChange={handlePageChange} />
             <div className="container">
                 <div className="row">
                     {page.content.map(movie => (
