eff3edae8db661e8f3ddadd8a29ee3b3dc8fd2db
falchizao <marcelonavarro11md@gmail.com>
2022-03-13 16:15:14 -0300

    Find movies

---
+++ backend/src/main/java/com/devsuperior/dsmovie/repositories/MovieRepository.java
@@ -0,0 +1,9 @@
+package com.devsuperior.dsmovie.repositories;
+
+import com.devsuperior.dsmovie.entities.Movie;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MovieRepository extends JpaRepository<Movie, Long>{
+    
+}
