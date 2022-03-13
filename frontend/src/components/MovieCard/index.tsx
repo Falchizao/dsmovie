2a6770be0c80afff2bbe5eaef1eca5be3c7a1afc
falchizao <marcelonavarro11md@gmail.com>
2022-03-13 20:44:57 -0300

    Show Score

--- frontend/src/components/MovieCard/index.tsx
+++ frontend/src/components/MovieCard/index.tsx
@@ -1,4 +1,4 @@
-import MovieScores from "components/MovieScores";
+import MovieScore from "components/MovieScores";
 import { Link } from "react-router-dom";
 import { Movie } from "types/movie";
 
@@ -6,20 +6,19 @@ type Props = {
     movie: Movie;
 }
 
-function MovieCard( { movie } : Props ) {
-
+function MovieCard( { movie } : Props) {
 
     return (
-
         <div>
             <img className="dsmovie-movie-card-image" src={movie.image} alt={movie.title} />
             <div className="dsmovie-card-bottom-container">
                 <h3>{movie.title}</h3>
-                <MovieScores /> 
+                <MovieScore count={movie.count} score={movie.score} />
+
                 <Link to={`/form/${movie.id}`}>
                     <div className="btn btn-primary dsmovie-btn">Avaliar</div>
                 </Link>
-                
+
             </div>
         </div>
     );
--- frontend/src/components/MovieScores/index.tsx
+++ frontend/src/components/MovieScores/index.tsx
@@ -1,19 +1,20 @@
-import MovieStars from 'components/MovieStars';
-import './styles.css'
+import MovieStars from "components/MovieStars";
+import './styles.css';
 
-function MovieScores() {
-    const score = 3.5;
-    const count = 13;
+type Props = {
+    score: number;
+    count: number;
+}
+
+function MovieScore({ score, count } : Props) {
 
     return (
         <div className="dsmovie-score-container">
             <p className="dsmovie-score-value">{score > 0 ? score.toFixed(1) : '-'}</p>
-            <MovieStars />
+            <MovieStars score={score} />
             <p className="dsmovie-score-count">{count} avaliações</p>
         </div>
-
-
     );
 }
 
-export default MovieScores
+export default MovieScore;
