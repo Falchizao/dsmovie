e0e1a2bf589cf623146f8ea4bf5389058ae5cbea
falchizao <marcelonavarro11md@gmail.com>
2022-03-13 23:57:44 -0300

    vamo q vamo

--- frontend/src/utils/requests.ts
+++ frontend/src/utils/requests.ts
@@ -1,1 +1,2 @@
-export const BASE_URL = process.env.REACT_APP_BACKEND_URL ?? "http://localhost:8080";
+export const BASE_URL = "https://falchi-dsmoviesproj.herokuapp.com"
+//process.env.REACT_APP_BACKEND_URL ?? "http://localhost:8080";
